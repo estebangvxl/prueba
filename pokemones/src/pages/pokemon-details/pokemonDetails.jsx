1c1f8fbdbf4772f6bca81958d49f42e3434f3de0
estebangvx <estebangvx@gmail.com>
2024-09-02 17:13:30 -0500

    correccion de error de key en lista

--- pokemones/src/pages/pokemon-details/pokemonDetails.jsx
+++ pokemones/src/pages/pokemon-details/pokemonDetails.jsx
@@ -16,14 +16,14 @@ const PokemonDetails = () => {
     console.log(pokemon)
 
     return (
-        <section class="page__pokemon_details">
+        <section className="page__pokemon_details">
             <img src={pokemon.image} alt="" />
             <ul className='list_details'>
                 <li className='name'> Name: {pokemon.name}</li>
                 <li>
-                    abilities: {pokemon.abilities.map((ability)=> <div className='sublist'>{ability.ability.name}</div>)}
+                    abilities: {pokemon.abilities.map((ability, index)=> <div key={index} className='sublist'>{ability.ability.name}</div>)}
                 </li>
-                <li>Types: {pokemon.types.map((type)=> <div className='sublist'>{type.type.name}</div>)}</li>
+                <li>Types: {pokemon.types.map((type, index)=> <div key={index} className='sublist'>{type.type.name}</div>)}</li>
             </ul>
         </section>
     );
