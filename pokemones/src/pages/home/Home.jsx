443b9b82456dd4053cae348e658b24ced62cae38
estebangvx <estebangvx@gmail.com>
2024-09-20 23:39:34 -0500

    refactorizacion componente home

--- pokemones/src/pages/home/Home.jsx
+++ pokemones/src/pages/home/Home.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import Searcher from '../../components/searcher/Searcher';
-import ListPokemons from '../../components/ListPokemons/ListPokemons';
-import { useState } from 'react';
+import SearchPokemon from '../search-pokemon/SearchPokemon';
 
 const Home = () => {
 
-    const [search, setSearch] = useState("");
-
     return (
         <>
-            <section className='page__home'>
-
-                <header className="header">
-                    <h1>Pokemones</h1>
-                    <Searcher change={(value) => {setSearch(value)}} />
-                </header>
-                <ListPokemons filter={search} />
-                
-            </section>
+            <SearchPokemon />
         </>
     );
 }
